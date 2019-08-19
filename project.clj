238b32825e7fa580c519a18402cae91029925d67
oakes <zach@sekao.net>
2019-08-19 10:16:18 -0400

    Initial import

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject paren-salad "0.1.0-SNAPSHOT"
+  :description "A library for tokenizing and Parinfering Clojure code"
+  :url "https://github.com/oakes/paren-salad"
+  :license {:name "Public Domain"
+            :url "http://unlicense.org/UNLICENSE"}
+  :repositories [["clojars" {:url "https://clojars.org/repo"
+                             :sign-releases false}]])
