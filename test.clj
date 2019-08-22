49512c9cccfb242a0d5a6afec5e60e87e4efa50a
oakes <zach@sekao.net>
2019-08-22 10:24:11 -0400

    Run smart mode tests

--- test.clj
+++ test.clj
@@ -3,18 +3,23 @@
   '[clojure.java.io :as io]
   '[parinferish.core :as ps])
 
-(doseq [file-name ["paren-mode.json"]
+(doseq [file-name ["smart-mode.json"]
         test-case (->> file-name
                        io/resource
                        slurp
                        json/read-str)
-        :when (empty? (get test-case "options"))]
-  (let [{:strs [text result]} test-case
-        opts {:parinfer :paren}
+        :let [{:strs [text result options]} test-case
+              cursor-line (get options "cursorLine")
+              cursor-column (get options "cursorX")]
+        :when (and cursor-line cursor-column)]
+  (let [opts {:mode :smart
+              :cursor-line cursor-line
+              :cursor-column cursor-column}
         ps-data (ps/parse text opts)
         ps-result (ps/flatten ps-data)]
     (when-not (= (get result "text") ps-result)
       (println "Test failed")
+      (println "Options: " opts)
       (println "Input:   " (pr-str text))
       (println "Output:  " (pr-str ps-result))
       (println "Expected:" (pr-str (get result "text")))
